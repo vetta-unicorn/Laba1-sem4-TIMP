eae8a65e6ed6555d82e3ad37dbdf33fb161fe3f2
Ko4erizhkas <fit12347@gmail.com>
2025-03-25 10:46:24 +0700

    Сделал страницу. В будущем возможны небольшие дополнения.

---
+++ Laba1 sem4 TIMP/BookV1.cpp
@@ -0,0 +1,14 @@
+#include "PageV1.cpp"
+#include <map>
+template <typename T>
+class BookV1
+{
+private:
+	std::map<size_t, Page<T>> book;
+
+public:
+	void CreateBookFile(std::string fileName)
+	{
+		std::ofstream fileOut ()
+	}
+};
--- Laba1 sem4 TIMP/PageV1.cpp
+++ Laba1 sem4 TIMP/PageV1.cpp
@@ -1,16 +1,11 @@
-#include "HelperClass.h"
+#include <iostream>
 #include <vector>
-#include <filesystem>
 #include <windows.h>
+#include <string>
+#include <fstream>
+#include <filesystem>
 
 #define MAX_SIZE_ARRAY 128
-#define DEBUG
-enum TypeArray
-{
-	T_INT,
-	T_CHAR,
-	T_VARCHAR
-};
 
 template <typename T>
 struct Page
@@ -24,11 +19,11 @@ struct Page
     std::time_t timeModify = 0;
 
      
-     //Пояснение к битовой карте//
+    //Пояснение к битовой карте//
     //Каждой странице предшествует битовая карта(массив битов),
     //в которой каждый бит соответствует ячейке моделируемого
     //массива, находящейся на странице. Значение бита 0 означает, что в эту ячейку ничего незаписано
-    std::vector<T> byteMap;
+    std::vector<byte> byteMap;
     size_t byteMapSize = MAX_SIZE_ARRAY;
 
     //Массив элементов
@@ -40,15 +35,15 @@ struct Page
     /// Переопределение оператора "<<" для простоты записи уже 
     /// готовой структуры в файл
     /// </summary>
-    static friend std::ofstream& operator << (std::ofstream& os, const Page<T>& page)
+    friend std::ofstream& operator << (std::ofstream& os, const Page<T>& page)
     {
-        for (int i = 0; i < page.byteMapSize; i++)
+        for (std::vector<byte>::const_iterator it = page.byteMap.cbegin(); it != page.byteMap.cend(); ++it)
         {
-            os << page.byteMap[i];
+            os << *it;
         }
-        for (int i = 0; i < page.elemArraySize; i++)
+        for (typename std::vector<T>::const_iterator it = page.elemArray.cbegin(); it != page.elemArray.cend(); ++it)
         {
-            os << page.elemArray[i];
+            os << *it;
         }
         os << page.numberPage;
         os << page.statusPage;
@@ -59,44 +54,14 @@ struct Page
     {
         this->numberPage = _numberPage;
         this->statusPage = _statusModify;
-
     }
-    ~Page(){    }
-    // Возвращает динамический массив размер которого равен количеству элементов исходного массива
-    // Пример: Имеется динамический массив размером 20, но в себе он хранит только 10 элементов
-    // функция вернёт тот-же массив, но размер его будет равен кол-ву элементов, т.е равен 10.
-    /*T* resizeArray(T* array)
+    Page(int _numberPage, bool _statusModify, const std::vector<byte> _byteMap, const std::vector<T> _elemArray)
     {
-        size_t sizeArray = 0;
-        for (int i = 0; i < MAX_SIZE_ARRAY; i++)
-        {
-            if (sizeof(array [i]) != 0)
-            {
-                sizeArray += 1;
-            }
-            else { break; }
-        }
-        
-        
-        T* copyArray = new T [sizeArray];
-        for (int i = 0; i < sizeArray; i++)
-        {
-            copyArray [i] = array [i];
-        }
-        array = copyArray;
-
-#ifdef DEBUG
-        std::cout << sizeArray << std::endl;
-        std::cout << "Массив с изменённым размером" << std::endl;
-        for (int i = 0; i < sizeArray; i++)
-        {
-            std::cout << array [i];
-        }
-        std::cout << std::endl;
-#endif
-        delete [] copyArray;
-        return array;
-    }*/
+        this->numberPage = _numberPage;
+        this->statusPage = _statusModify;
+        this->byteMap = AddByteMap(_byteMap);
+        this->elemArray = AddElements(_elemArray);
+    }
     void AddElemInArray(std::vector<T> _elemArray)
     {
         if (elemArray != NULL)
@@ -108,12 +73,29 @@ struct Page
             throw std::exception("");
         }
     }
+    void AddByteMap(const std::vector<byte>& vec)
+    {
+        byteMap.insert(byteMap.end(), vec.begin(), vec.end());
+    }
+    void AddElements (const std::vector<T>& vec)
+    {
+        elemArray.insert(elemArray.end(), vec.begin(), vec.end());
+    }
+    void PrintByteMap()
+    {
+        for (std::vector<byte>::iterator it = byteMap.begin(); it != byteMap.end(); ++it)
+        {
+            std::cout << (int)*it;
+        }
+        std::cout << std::endl;
+    }
     void PrintElemArray()
     {
-        for (int i = 0; i < MAX_SIZE_ARRAY; i++)
+        for (typename std::vector<T>::iterator it = elemArray.begin(); it != elemArray.end(); ++it)
         {
-            std::cout << elemArray[i];
+            std::cout << *it;
         }
+        std::cout << std::endl;
     }
     size_t getTotalSize()
     {
@@ -122,11 +104,11 @@ struct Page
         
         for (int i = 0; i < MAX_SIZE_ARRAY; i++)
         {
-            totalSizeByteMap += sizeof(byteMap[i]);
-            totalSizeElemArray += sizeof(elemArray[i]);
+            totalSizeByteMap += sizeof(byteMap);
+            totalSizeElemArray += sizeof(elemArray);
         }
 
-        return sizeof(Page<T>) + totalSizeByteMap;// +totalSizeElemArray;
+        return sizeof(Page<T>) + totalSizeByteMap + totalSizeElemArray;
     }
 
     /// <summary>
@@ -147,159 +129,52 @@ struct Page
         std::strftime(buffer, sizeof(buffer), "%d.%m.%y %H:%M:%S", localTime);
         return std::string(buffer);
     }
-};
-//template <typename T>
-//class WorkerClass
-//{
-//private:
-//
-//	HelperClass help;
-//	Page<T> bufferPage;
-//
-//    std::vector<Page<T>> dataBlocks;
-//
-//	TypeArray typeArray;
-//	int sizeArray;
-//	int lenString = 0;
-//
-//    // Пока не трогать, т.к не понятно, пригодится ли оно или нет!
-//	/*T WriteInFile(std::string _fileName, size_t _sizeArray,
-//				  TypeArray _typeArray, size_t _lenArray)
-//	{
-//		std::ofstream fileOut(_fileName, std::ios::in | std::ios::out);
-//		if (!fileOut.is_open())
-//		{
-//			help.CreateFileH(_fileName);
-//			bufferPage = Page<T>{};
-//		}
-//		if (_typeArray == T_INT)
-//		{
-//			bufferPage = Page<int>();
-//            bufferPage.SetArraySize(_sizeArray);
-//
-//
-//		}
-//		if (_typeArray == T_CHAR)
-//		{
-//			bufferPage = Page<char>();
-//		}
-//        if (_typeArray == T_VARCHAR)
-//        {
-//            bufferPage = Page<std::string>();
-//        }
-//	}*/
-//
-//public:
-//	WorkerClass(std::string _fileName, size_t _sizeArray, 
-//				TypeArray _typeArray, size_t _lenArray = 0)
-//	{
-//		
-//		this->lenString = _lenArray;
-//		this->sizeArray = _sizeArray;
-//		this->typeArray = _typeArray;
-//        
-//		if (!(std::filesystem::exists(_fileName)))
-//		{
-//            help.CreateFileH(_fileName);
-//            std::ofstream fileOut(_fileName, std::ios::in | std::ios::out);
-//            fileOut << bufferPage;
-//            fileOut.close();
-//		}
-//		
-//	}
-//	T WriteInFile(std::string& _fileName, const Page<T>& page)
-//	{
-//		std::ofstream fileOut(_fileName, std::ios::in | std::ios::out);
-//
-//		if (!fileOut.is_open())
-//		{
-//			throw std::exception("Cannot open file!");
-//		}
-//
-//		if (page == NULL)
-//		{
-//			throw std::invalid_argument("Argument cannot equals NULL!");
-//		}
-//		else
-//		{
-//			fileOut << page;
-//		}
-//		return 0;
-//	}
-//	T WriteInFile(std::string fileName)
-//	{
-//
-//	}
-//
-//    T CreateNewPage()
-//    {
-//
-//    }
-//};
-
-
-class adad
-{
-public:
-    adad() {}
-    void TestStruct()
+    void printVector(const std::vector<T>& vec)
     {
-        SetConsoleCP(1251);
-        SetConsoleOutputCP(1251);
-
-        Page<char> page(1, false);
-        std::time_t times = std::time(0);
-
-
-        const std::vector<char> elem{ '1','2','3' };
-        const std::vector<byte> temp{ 1,0 };
-
-        for (auto i = 0; i < elem.begin(); i++)
+        for (auto it = vec.cbegin(); it != vec.cend(); ++it)
         {
-            std::cout << elem[i];
+            std::cout << *it;
         }
-        for (auto i = 0; i < temp.begin(); i++)
-        {
-            std::cout << temp [i];
-        }
-        page.timeModify = times;
-
-
-        // Запись в файл
-        std::ofstream file("adada.txt", std::ios::out);
-        if (file.is_open())
-        {
-            file << page;
-            file.close();
-        }
-        else
-        {
-            std::cout << "Не удалось открыть файл\n";
-        }
-
-        std::cout << page.getTotalSize() << " (размер страницы в байтах)" << std::endl;
-        std::cout << "Номер страницы: " << page.numberPage << std::endl;
-        std::cout << "Статус: " << (page.statusPage ? "true" : "false") << std::endl;
-        std::cout << "Время создания/изменения" << page.getFormattedTime() << std::endl;
-        std::cout << "Массив byteMap: ";
-        for (int i = 0; i < page.byteMapSize; i++)
-        {
-            std::cout << page.byteMap [i];
-        }
-
-        std::cout << std::endl;
-        for (int i = 0; i < page.elemArraySize; i++)
-        {
-
-        }
-        std::cout << std::endl;
-        page.PrintElemArray();
     }
 };
-void TestClass(){}
-int main()
+
+
+void TestStruct()
 {
-    adad d;
-    d.TestStruct();
-    return 0;
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    Page<char> page(1, false);
+    std::time_t times = std::time(0);
+
+
+    const std::vector<byte> byteMap = { 1,0,0 };
+
+    const std::vector<char> elem = { 'a','b','c' };
+    
+    page.timeModify = times;
+
+    page.AddByteMap(byteMap);
+    page.AddElements(elem);
+
+    // Запись в файл
+    std::ofstream file("adada.txt", std::ios::out);
+    if (file.is_open())
+    {
+        file << page;
+        file.close();
+    }
+    else
+    {
+        std::cout << "Не удалось открыть файл\n";
+    }
+
+    std::cout << page.getTotalSize() << " (размер страницы в байтах)" << std::endl;
+    std::cout << "Номер страницы: " << page.numberPage << std::endl;
+    std::cout << "Статус: " << (page.statusPage ? "true" : "false") << std::endl;
+    std::cout << "Время создания/изменения: " << page.getFormattedTime() << std::endl;
+    std::cout << "Массив byteMap: ";
+    page.PrintByteMap();
+    page.PrintElemArray();
+    std::cout << std::endl;
 }
